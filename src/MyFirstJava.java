6d788a9e2ac2db574f4a54b38a97accf2af039fa
tsadio <tsadio@montgomerycollege.edu>
2020-04-13 14:04:50 -0400

    first commit

---
+++ src/MyFirstJava.java
@@ -0,0 +1,10 @@
+
+/*
+This is the first Java program which will print out a message. Hello world
+ */
+
+public class MyFirstJava {
+    public static void main (String[] args) {
+        System.out.println("Hello world.");
+    }
+}
